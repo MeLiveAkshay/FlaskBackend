753e5e77f7b45a7ada4dc3d659bc7e332826ab5f
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-04-24 22:03:02 +0530

    class 1

--- app.py
+++ app.py
@@ -1,30 +1,9 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 app = Flask(__name__)
- app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-
-#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://username:password@localhost/databasename"
-
-db = SQLAlchemy(app)
-
-
-#model
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(unique=True)
-    email: Mapped[str]
 
 @app.route('/home')
 def home():
     return 'Hello, World!'
-
-
-@app.route('/')
-def index():
-    return render_template('index.html',name="Akshay")
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/index.js
+++
@@ -1,3 +0,0 @@
-const hello= document.querySelector('h1');
-hello.style.color = 'red';
-hello.textContent = 'Hello World';
