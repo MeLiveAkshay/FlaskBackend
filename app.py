c094005933e231d4a4db84dd1e6a982e80429d81
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-04-24 22:05:19 +0530

    class 2

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ app = Flask(__name__)
 
 @app.route('/home')
 def home():
-    return render_template('index.html')
-
+    return render_template('index.html',context="Home, I am here to help us for the going to the account help")
 if __name__ == '__main__':
     app.run(debug=True)
