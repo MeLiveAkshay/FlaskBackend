427b795ed6a7f49e4adbe53757fd4adb29dfcecc
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-04-24 22:02:00 +0530

    complete

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 
+#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://username:password@localhost/databasename"
+
+db = SQLAlchemy(app)
+
+
+#model
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str]
 
 @app.route('/home')
 def home():
